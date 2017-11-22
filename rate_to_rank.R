0dcbcf7a167e33a201ef603ff5cfd5aeca28123a
suziebrown <suzie.brown42@gmail.com>
2017-11-22 11:56:16 +0000

    inverted indices of ranking to match better the ratings data format.

--- rate_to_rank.R
+++ rate_to_rank.R
@@ -2,10 +2,12 @@
 #' 
 #' @param ratings The matrix of ratings as returned in the first list item from make.ratings
 #'
-#' @return matrix where each column contains the ranking by one rater. e.g. "2 3 1 4" means 2 was ranked first, 3 second etc.
+#' @return matrix where each column contains the ranking by one rater. e.g. "2 3 1 4" means alternative 1 was ranked second, 2 was ranked third, etc.
 #'
 #' @export ratings2ranking
 
 ratings2ranking <- function(ratings) {
-  apply(ratings, 2, order, decreasing=TRUE)
+  invrank <- apply(ratings, 2, order, decreasing=TRUE)
+  rank <- apply(invrank, 2, order)
+  rank
 }
