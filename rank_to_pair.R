2a126b725c2359cdddf266cd782c79fd39a64edc
suziebrown <suzie.brown42@gmail.com>
2017-11-22 12:13:45 +0000

    function to convert rankings to summary of pairwise comparisons

---
+++ rank_to_pair.R
@@ -0,0 +1,16 @@
+#' Convert rankings to pairwise comparisons
+#'
+#' @param ranking a matrix of rankings, as returned by ratings2ranking
+#' 
+#' @return a matrix W where w_{i,j} is the number of rankings in which i beat j
+
+ranking2pairs <- function(ranking) {
+  n.alts <- nrow(ranking)
+  w <- matrix(NA, nrow=n.alts, ncol=n.alts)
+  for (i in 1:n.alts) {
+    for (j in 1:n.alts) {
+      w[i,j] <- sum(ranking[i,]<ranking[j,])
+    }
+  }
+  w
+}
